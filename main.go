3b37ad80ebdfbf03ae2db0cf1554ce18ca45db92
Florian Margaine <florian@margaine.com>
2013-06-02 19:30:08 +0200

    Trying to use waitgroups

--- main.go
+++ main.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"cssminify"
+	"sync"
 )
 
 func main() {
 	files := cssminify.Files()
 	cb := make(chan cssminify.Block)
 
-	go getBlocks(cb, files)
-	go cssminify.Minify(cb)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go getBlocks(cb, files, wg)
+	wg.Wait()
+
+	wg.Add(1)
+	go cssminify.Minify(cb, wg)
+	wg.Wait()
 }
 
-func getBlocks(cb chan cssminify.Block, files []string) {
+func getBlocks(cb chan cssminify.Block, files []string, wg sync.WaitGroup) {
 	for _, file := range files {
-		cssminify.Blocks(cb, file)
+		cssminify.Blocks(cb, file, wg)
 	}
+	defer wg.Done()
 }
