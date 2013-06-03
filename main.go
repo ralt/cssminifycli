6bf1fa59f5fee0644b31c39a6c2c5b732ee8c403
Florian Margaine <florian@margaine.com>
2013-06-03 20:50:19 +0200

    Reverting channels usage

--- main.go
+++ main.go
@@ -2,26 +2,11 @@ package main
 
 import (
 	"cssminify"
-	"sync"
 )
 
 func main() {
 	files := cssminify.Files()
-	cb := make(chan cssminify.Block)
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go getBlocks(cb, files, wg)
-	wg.Wait()
-
-	wg.Add(1)
-	go cssminify.Minify(cb, wg)
-	wg.Wait()
-}
-
-func getBlocks(cb chan cssminify.Block, files []string, wg sync.WaitGroup) {
 	for _, file := range files {
-		cssminify.Blocks(cb, file, wg)
+		cssminify.Minify(cssminify.Blocks(file))
 	}
-	wg.Done()
 }
